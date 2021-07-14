1bd7ac2f6af74500cbaa5b6ecb73d8313849dc6d
Jorge-Mira <jorge@gbyteinfo.com.br>
2021-07-14 20:00:28 -0300

    Sidebar Config

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import MainGrid from '../src/components/MainGrid' //CSS
 import Box from '../src/components/Box' //CSS
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AluraCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AluraCommons'
 import { AreaProfileRelationsBoxWrapper } from '../src/components/AreaProfileRelations'
+
 
 function ProfileSidebar(props){
   console.log("Debung do Index props => ", props)
   return (
     <Box>
       <img src={`https://github.com/${props.userThumb}.png`} style={{borderRadius: '8px'}}/>
+      <hr />
+        <p>
+          <a className ="boxLink" href={`https://github.com/${props.userThumb}`}>
+            @{props.userThumb}
+          </a>
+        </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
-  
   const githubUser = 'gbyteinfo';
   const pessoasFavoritas = ['gbyteinfo', 'juunegreiros', 'omariosouto', 'peas', 'joana', 'felipefialho']
-  
   return ( 
     <>
       <AlurakutMenu />
@@ -37,26 +44,22 @@ export default function Home() {
         
         <div className="areaProfileRelations" style={{gridArea: 'areaProfileRelations'}}>
           <AreaProfileRelationsBoxWrapper>
-          <h2 className="smallTitle">Comunidade GitHub ({pessoasFavoritas.length})</h2>
-            <ul style={{margin:"0px"}}>
-              {pessoasFavoritas.map((pessoasMapeadas) => {
-                return (
-                  <li>
-                    <a href={`/users/${pessoasMapeadas}`} key={pessoasMapeadas}>
-                      <img src={`https://github.com/${pessoasMapeadas}.png`}/>
-                      <span>{pessoasMapeadas}</span>
-                    </a>
-                  </li>
-                )})
-              }{/*pessoasFavoritas*/}
-            </ul>
+            <h2 className="smallTitle">Comunidade GitHub ({pessoasFavoritas.length})</h2>
+              <ul style={{margin:"0px"}}>
+                {pessoasFavoritas.map((pessoasMapeadas) => {
+                  return (
+                    <li>
+                      <a href={`/users/${pessoasMapeadas}`} key={pessoasMapeadas}>
+                        <img src={`https://github.com/${pessoasMapeadas}.png`}/>
+                        <span>{pessoasMapeadas}</span>
+                      </a>
+                    </li>
+                  )})
+                }{/*pessoasFavoritas*/}
+              </ul>
             </AreaProfileRelationsBoxWrapper>
         </div>
 
-
-
-
-        
       </MainGrid>
     </>
   )
